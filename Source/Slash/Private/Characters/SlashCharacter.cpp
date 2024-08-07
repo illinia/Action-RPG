2297e352ef6c442ad9cf5fc235d9407f1264403d
TAEMIN <seolin1218@gmail.com>
2024-08-07 23:38:32 +0900

    Enhanced Input

--- Source/Slash/Private/Characters/SlashCharacter.cpp
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "Characters/SlashCharacter.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 // Sets default values
 ASlashCharacter::ASlashCharacter()
@@ -11,11 +14,64 @@ ASlashCharacter::ASlashCharacter()
 
 }
 
+void ASlashCharacter::Jump()
+{
+	Super::Jump();
+}
+
 // Called when the game starts or when spawned
 void ASlashCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(SlashContext, 0);
+		}
+	}
+}
+
+void ASlashCharacter::Move(const FInputActionValue& Value)
+{
+	// if (ActionState != EActionState::EAS_Unoccupied) return;
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	const FRotator Rotation = Controller->GetControlRotation();
+	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	AddMovementInput(ForwardDirection, MovementVector.Y);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	AddMovementInput(RightDirection, MovementVector.X);
+
+	// const FVector Forward = GetActorForwardVector();
+	// AddMovementInput(Forward, MovementVector.Y);
+	//
+	// const FVector Right = GetActorRightVector();
+	// AddMovementInput(Right, MovementVector.X);
+}
+
+void ASlashCharacter::Look(const FInputActionValue& Value)
+{
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddControllerPitchInput(LookAxisVector.Y);
+	AddControllerYawInput(LookAxisVector.X);
+}
+
+void ASlashCharacter::EKeyPressed()
+{
+}
+
+void ASlashCharacter::Attack()
+{
+}
+
+void ASlashCharacter::Dodge()
+{
 }
 
 void ASlashCharacter::MoveForward(float Value)
@@ -39,6 +95,16 @@ void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ASlashCharacter::MoveForward);
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Look);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Jump);
+		EnhancedInputComponent->BindAction(EKeyAction, ETriggerEvent::Triggered, this, &ASlashCharacter::EKeyPressed);
+		EnhancedInputComponent->BindAction(AttackAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Attack);
+		EnhancedInputComponent->BindAction(DodgeAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Dodge);
+	}
+
+	// PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ASlashCharacter::MoveForward);
 }
 
--- Source/Slash/Public/Characters/SlashCharacter.h
+++ Source/Slash/Public/Characters/SlashCharacter.h
@@ -3,8 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/Character.h"
 #include "SlashCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class SLASH_API ASlashCharacter : public ACharacter
@@ -15,9 +19,38 @@ public:
 	// Sets default values for this character's properties
 	ASlashCharacter();
 
+	virtual void Jump() override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputMappingContext* SlashContext;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* EKeyAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* AttackAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	UInputAction* DodgeAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void EKeyPressed();
+	virtual void Attack();
+	void Dodge();
 
 	void MoveForward(float Value);
 
