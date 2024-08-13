6d367e412166692652e40cf4131f0f96adcd5b54
TAEMIN <seolin1218@gmail.com>
2024-08-13 22:24:13 +0900

    Switching Animation Poses

--- Source/Slash/Public/Characters/SlashAnimInstance.h
+++ Source/Slash/Public/Characters/SlashAnimInstance.h
@@ -33,5 +33,6 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category=Movement)
 	bool IsFalling;
 
+	UPROPERTY(BlueprintReadOnly, Category="Movement | Character State")
 	ECharacterState CharacterState;
 };
