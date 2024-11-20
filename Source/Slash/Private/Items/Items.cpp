7e6a2601635c71cb8d0358c1e77dab435098854a
Taemin <seolin1218@gmail.com>
2024-11-20 22:05:02 +0900

    Actor Creation

---
+++ Source/Slash/Private/Items/Items.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Items.h"
+
+AItems::AItems()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItems::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItems::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
