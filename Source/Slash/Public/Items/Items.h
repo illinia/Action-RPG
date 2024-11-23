4a675b9e2669c8a5627fb45d70a862157d475e82
Taemin <seolin1218@gmail.com>
2024-11-23 20:00:41 +0900

    Visible But Not Editable

--- Source/Slash/Public/Items/Items.h
+++ Source/Slash/Public/Items/Items.h
@@ -18,11 +18,12 @@ public:
 protected:
 	virtual void BeginPlay() override;
 private:
+	UPROPERTY(VisibleAnywhere)
 	float RunningTime;
 
 	UPROPERTY(EditAnywhere)
 	float Amplitude = 0.25f;
 
-	UPROPERTY(EditInstanceOnly)
+	UPROPERTY(EditAnywhere)
 	float TimeConstant = 5.f;
 };
