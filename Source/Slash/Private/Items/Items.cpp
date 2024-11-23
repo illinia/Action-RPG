6276f8e1438331dd861b6a432224dc9f49b2ca11
Taemin <seolin1218@gmail.com>
2024-11-23 23:12:40 +0900

    Components in C++

--- Source/Slash/Private/Items/Items.cpp
+++ Source/Slash/Private/Items/Items.cpp
@@ -10,6 +10,9 @@
 AItems::AItems()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
 }
 
 void AItems::BeginPlay()
@@ -32,15 +35,6 @@ void AItems::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	RunningTime += DeltaTime;
-	
-	// float DeltaZ = Amplitude * FMath::Sin(RunningTime * TimeConstant);
-
-	// AddActorWorldOffset(FVector(0.f, 0.f, DeltaZ));
-	
-	DRAW_SPHERE_SingleFrame(GetActorLocation());
-	DRAW_VECTOR_SingleFrame(GetActorLocation(), GetActorLocation() + GetActorForwardVector() * 100.f)
-
-	FVector AvgVector = Avg<FVector>(GetActorLocation(), FVector::ZeroVector);
-	DRAW_POINT_SingleFrame(AvgVector);
 }
+
 
