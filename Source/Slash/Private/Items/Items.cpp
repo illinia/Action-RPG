8833e451b706191ef8baf596827d2140813d157c
Taemin <seolin1218@gmail.com>
2024-11-23 15:50:08 +0900

    Draw Debug Lines

--- Source/Slash/Private/Items/Items.cpp
+++ Source/Slash/Private/Items/Items.cpp
@@ -18,32 +18,16 @@ void AItems::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Begin Play Called!"))
-
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
-	}
-
 	UWorld* World = GetWorld();
-
 	FVector Location = GetActorLocation();
+	FVector Forward = GetActorForwardVector();
+
 	DRAW_SPHERE(Location)
+	DRAW_LINE(Location, Location + Forward * 100.f)
 }
 
 void AItems::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
-	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime)
-
-	if (GEngine)
-	{
-		FString Name = GetName();
-		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
-		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
-
-		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name)
-	}
 }
 
